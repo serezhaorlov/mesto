47baf5f229261361bf57603e2a108ae9a8d062ed
Serezha Orlov <somepuppeys@yandex.ru>
2020-09-28 20:55:30 +0300

    Доделана верстка, JS-часть

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let popUpOpenButton = document.querySelector(".profile__edit-button");
+let popUpCloseButton = document.querySelector(".form__close-button");
+let popUp = document.querySelector(".popup");
+
+let popUpToggle = () => {
+  popUp.classList.toggle("popup_is-opened");
+}
+
+popUpOpenButton.addEventListener("click", popUpToggle);
+popUpCloseButton.addEventListener("click", popUpToggle);
+
+let formElement = document.querySelector('.form');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let nameInput =  document.querySelector('.form__name');
+    let jobInput = document.querySelector('.form__subname');
+
+    let profileName = document.querySelector('.profile__name');
+    let profileSubInfo = document.querySelector('.profile__sub-info');
+
+    profileName.textContent = nameInput.value;
+    profileSubInfo.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
