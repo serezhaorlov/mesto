7b19fc46cf881b9d6f0af74f83e045accc93fe27
Serezha Orlov <somepuppeys@yandex.ru>
2020-09-29 21:33:41 +0300

    Внесены правки по стилям, разметке, и коду JS

--- scripts/index.js
+++ scripts/index.js
@@ -2,34 +2,33 @@ let popUpOpenButton = document.querySelector(".profile__edit-button");
 let popUpCloseButton = document.querySelector(".form__close-button");
 let popUp = document.querySelector(".popup");
 
-let popUpToggle = () => {
-  popUp.classList.toggle("popup_is-opened");
-}
-
-popUpOpenButton.addEventListener("click", popUpToggle);
-popUpCloseButton.addEventListener("click", popUpToggle);
-
+let formElement = document.querySelector('.form');
+let nameInput =  document.querySelector('.form__name');
+let jobInput = document.querySelector('.form__name_bottom');
 let saveButtonPopUpClose = document.querySelector('.form__button')
 
-let popUpClose = () => {
-  popUp.classList.remove("popup_is-opened");
-}
-saveButtonPopUpClose.addEventListener("click", popUpClose);
+let profileName = document.querySelector('.profile__name');
+let profileSubInfo = document.querySelector('.profile__sub-info');
 
-let formElement = document.querySelector('.form');
+
+let popUpToggle = () => {
+  popUp.classList.toggle("popup_is-opened");
+  if (popUp.classList.contains("popup_is-opened")){
+    nameInput.value = profileName.textContent; //подправил, теперь данные передаются из name в форму
+    jobInput.value = profileSubInfo.textContent;
+  }
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let nameInput =  document.querySelector('.form__name');
-    let jobInput = document.querySelector('.form__subname');
-
-    let profileName = document.querySelector('.profile__name');
-    let profileSubInfo = document.querySelector('.profile__sub-info');
-
     profileName.textContent = nameInput.value;
     profileSubInfo.textContent = jobInput.value;
 }
 
+
+popUpOpenButton.addEventListener("click", popUpToggle);
+popUpCloseButton.addEventListener("click", popUpToggle);
+saveButtonPopUpClose.addEventListener("click", popUpToggle);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
