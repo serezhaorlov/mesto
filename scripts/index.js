be6513c5c26b867160d17316c7e1ff7deda8663f
Serezha Orlov <somepuppeys@yandex.ru>
2020-10-01 14:11:31 +0300

    Исправить исправления исправленного

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ let popUpCloseButton = document.querySelector(".form__close-button");
 let popUp = document.querySelector(".popup");
 
 let formElement = document.querySelector('.form');
-let nameInput =  document.querySelector('.form__name');
-let jobInput = document.querySelector('.form__name_bottom');
+let nameInput =  document.querySelector('.form__name_top'); //решил через модификаторы
+let jobInput = document.querySelector('.form__name_bottom'); //но логичней сделать по идее и вправду через hasAttribute, я так понимаю, что правильного решения нет и я деалю как мне удобней или все атки рациональней сделать на атрибутах проверку?
 let saveButtonPopUpClose = document.querySelector('.form__button')
 
 let profileName = document.querySelector('.profile__name');
@@ -23,11 +23,10 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileSubInfo.textContent = jobInput.value;
-    popUpToggle(); //вышло кайфово 8)
+    popUpToggle();
 }
 
 popUpOpenButton.addEventListener("click", popUpToggle);
 popUpCloseButton.addEventListener("click", popUpToggle);
 formElement.addEventListener('submit', formSubmitHandler);
-
 
