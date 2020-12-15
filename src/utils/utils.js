8bba3b9bf2156eb3f9c135e6d0d43566d24e677b
Serezha Orlov <somepuppeys@yandex.ru>
2020-12-15 12:54:21 +0300

    added all

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
-  export const loadingButtonState = (state) =>{ //сделал
-    if(state) {
-      const popupIsOpened = document.querySelector('.popup_is-opened')
-      popupIsOpened.querySelector('.form__button').textContent = 'Сохранение...';
-    } else {
-      const popupIsOpened = document.querySelector('.popup_is-opened')
-      popupIsOpened.querySelector('.form__button').textContent = 'Сохранение';
-    }
+export const loadingButtonState = (state) =>{ //сделал
+  if(state) {
+    const openedButton = document.querySelector('.popup_is-opened .form__button')
+    openedButton.textContent = 'Сохранение...'
+  } else {
+    const openedButton = document.querySelector('.popup_is-opened .form__button')
+    openedButton.textContent = 'Сохранение';
   }
+}
 
